c1a0e91a2f61e2816bf723ff0a2a0c07eb223f1c
Ankushch27 <ankushch2712@gmail.com>
2022-01-14 11:25:45 +0530

    Link modified to support dynamic profile pages

--- components/ui/Link.tsx
+++ components/ui/Link.tsx
@@ -1,4 +1,5 @@
 import cn from 'classnames'
+import { useSession } from 'next-auth/react'
 import NextLink, { LinkProps as NextLinkProps } from 'next/link'
 import { useRouter } from 'next/router'
 import { FC } from 'react'
@@ -16,7 +17,12 @@ const Link: FC<LinkProps> = ({
   ...props
 }) => {
   const { pathname } = useRouter()
-  const isActive = pathname === href && activeClassName
+  const { data: session } = useSession()
+  const username = session?.user.username
+  
+  let p = pathname.includes('[profile]') ? pathname.replace('[profile]', `${username}`) : pathname
+  
+  const isActive = p === href && activeClassName
   const linkClasses = cn(className, { [activeClassName]: isActive })
 
   return (
